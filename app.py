d7d38deee68b2db6880ecb92eea08c4c55d65a86
shinsad0907 <112173054+shinsad0907@users.noreply.github.com>
2025-03-26 11:38:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, flash, jso
 from functools import wraps
 from datetime import datetime
 from src_python.get_data import GET_DATA
+import os
 app = Flask(__name__)
 app.secret_key = 'supersecretkey'
 
